4ec575b7f4690dcf28c501ecb05dc218d731388a
Tim Mok <timothyjmok@gmail.com>
2021-03-30 12:40:33 -0400

    finished the exercise

--- part-2.sql
+++ part-2.sql
@@ -1,1 +1,8 @@
 -- Add the appropriate foreign key to the appropriate table
+
+CREATE TABLE properties (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    units INT,
+    ownerID INT references owners(id)
+);
--- part-3.sql
+++ part-3.sql
@@ -1,4 +1,24 @@
 -- Insert Data into the owners table
 
+INSERT INTO owners
+(name, age)
+VALUES
+('William', 29),
+('Jane', 43),
+('Yuki', 67),
+('Tim', 35),
+('Timothy', 35),
+('Timotheus', 35);
+
+
 -- Insert Data into the properties table
 
+INSERT INTO properties
+(name, units, ownerID)
+VALUES
+('Archstone', 20, 3),
+('Zenith Hills', 10, 3),
+('Willowspring', 30, 2),
+('The Major', 40, 4),
+('The Magnificent', 50, 5),
+('The Massive', 60, 6);
